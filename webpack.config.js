b0c9d18781d44c9049a86cb4f9db70032513cf5b
Siming Dong <clovedsm@gmail.com>
2022-02-26 20:18:36 +1100

    2nd commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
 const path = require('path')
 
 module.exports = {
+  entry: { index: './src/index.js' },
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
   },
-  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist/assets'),
   },
-}
-
-module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
-    },
-  ]
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
